abd9cddbf63c72a5c5a0964bdb108d0ef8c0c89f
Christophe Blaess <christophe@blaess.fr>
2014-12-14 15:37:13 +0100

    First github release

---
+++ exemples/chapitre-03/fact.h
@@ -0,0 +1,6 @@
+#ifndef LIB_FACT_H
+#define LIB_FACT_H
+	long int  factorielle(long int n);
+#endif
+
+
---
+++ exemples/chapitre-06/exemple-02.c
@@ -0,0 +1,49 @@
+
+#define _GNU_SOURCE  // sched_getcpu() est une extension GNU
+#include <pthread.h>
+#include <sched.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include <sys/time.h>
+
+
+void * fonction_thread(void * arg)
+{
+	int cpu = (int) arg;
+	cpu_set_t cpu_set;
+	struct sched_param param;
+
+	CPU_ZERO(& cpu_set);
+	CPU_SET(cpu, & cpu_set);
+	if (pthread_setaffinity_np(pthread_self(), sizeof(cpu_set), & cpu_set) != 0) {
+		perror("pthread_setaffinity_np");
+		exit(EXIT_FAILURE);
+	}
+
+	param.sched_priority = 99;
+	if (pthread_setschedparam(pthread_self(), SCHED_FIFO, & param) != 0) {
+		perror("pthread_setschedparam");
+		exit(EXIT_FAILURE);
+	}
+
+	fprintf(stderr, "Thread sur CPU %d va demarrer...\n", cpu);
+	sleep(1);
+	while (1)
+		;
+	return NULL;
+}
+
+
+int main(void)
+{
+	int cpu;
+	pthread_t thr;
+
+	alarm(15);
+	for (cpu = 0; cpu < sysconf(_SC_NPROCESSORS_ONLN); cpu ++)
+		pthread_create (& thr, NULL, fonction_thread, (void *) cpu);
+        pthread_exit(NULL);
+}
+
